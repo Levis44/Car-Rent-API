fe6a8b1bc637a47da7be21746eaf064625c8c029
Levi <levi.ciarrocchi@gmail.com>
2021-05-31 13:03:30 -0300

    Categories routes post and get

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlredyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlredyExists) {
+    return response.status(400).json({ error: 'Category Alredy Exists' });
+  }
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello' });
-});
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(3000, () => console.log('Server running'));
